4596ea2a9486a3492726aa4ff5771c3defb667bf
Pushkar Tiwari <pushkartiwari6@gmail.com>
2022-10-16 23:25:45 +0530

    made changes in about and home

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
 function About(){
     return <div>
         <h1>About Page</h1>
+        <p>This is the about page awesome App</p>
+        <p1>And here we are learning about router <br/>  </p1>
+        <Link to="/">Go to Home</Link>
 
     </div>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home(){
     return (<div>
         <h1>Home Page content</h1>
-
+        <p>This is the home page awesome App</p>
+        <p1>And here we are learning about router <br/>  </p1>
+        <Link to="/about">Go to about page</Link>
     </div>
     )
 
