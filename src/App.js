be33dd57bfbac3d56186bd7f54e2fea8473a993c
Pushkar Tiwari <pushkartiwari6@gmail.com>
2022-10-16 23:25:17 +0530

    Second commit Link Routes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      React router dom!!!
+      <BrowserRouter>
+        <NavBar/>
+      
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About/>} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home(){
+    return (<div>
+        <h1>Home Page content</h1>
+
+    </div>
+    )
+
+}
+
+export default Home;
